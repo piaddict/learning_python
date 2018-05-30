33d1b6e270680689ae4ef19a5fe309768a673e3b
Sangdon Park <pravusid@gmail.com>
2018-05-30 21:55:36 +0900

    update: selection sort - variable swap

--- algorithm/selection_sort.py
+++ algorithm/selection_sort.py
@@ -1,13 +1,11 @@
 def SelectionSort(list):
     size = len(list)
-    for element in range(0, size): # range는 반폐구간
+    for element in range(size): # range는 반폐구간
         min_position = element
         for target in range(element + 1, size):
             if (list[target] < list[min_position]):
                 min_position = target
-        temp = list[element]
-        list[element] = list[min_position]
-        list[min_position] = temp
+        (list[element], list[min_position]) = (list[min_position], list[element])
 
 alist = [30, 50, 7, 40, 88, 15, 44, 55, 22, 33, 77, 90, 11, 66, 1, 85]
 SelectionSort(alist)
